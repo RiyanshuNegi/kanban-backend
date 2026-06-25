3e61e985af385b8d377240e09df2dfdd730298a9
RiyanshuNegi <rn7msd@gmail.com>
2026-06-25 15:44:56 +0530

    for vercel

--- src/server.js
+++ src/server.js
@@ -19,9 +19,9 @@ connectDB();
 
 // 3. Start the Server
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`);
-});
+// const server = app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`);
+// });
 
 // 4. Safety Net: Catch asynchronous promise rejections (e.g., DB connection loss)
 process.on('unhandledRejection', (err) => {
